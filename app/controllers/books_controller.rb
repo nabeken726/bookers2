9e63ea32c87d9ad869475cd663eedfa6f29cafe6
EC2 Default User <ec2-user@ip-172-31-40-136.ec2.internal>
2022-05-05 07:36:53 +0000

    Homeページ編集

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,11 @@
+class BooksController < ApplicationController
+  def new
+    @book = book.new
+  end
+
+  def index
+  end
+
+  def top
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  get 'users/edit'
+  
+  root to: "homes#top"
+  devise_for :users
+  
+  resources :books, only: [:new, :index, :show]
+  
+
+
+   get 'homes/top' => 'homes#top',as:'top'
+   get 'homes/about' => 'homes#about',as:'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
