ecca2bf2bec18ddaddf3c43e8df40cfe4e36fc17
EC2 Default User <ec2-user@ip-172-31-40-136.ec2.internal>
2022-05-09 10:46:28 +0000

    Bootstrap install

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,14 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'users/edit'
-  
   root to: "homes#top"
   devise_for :users
-  
-  resources :books, only: [:new, :index, :show]
-  
 
+  resources :books, only: [:index, :show, :create, :edit, :destroy, :update]
+  resources :users, only: [:index, :show, :create, :edit, :update ]
 
    get 'homes/top' => 'homes#top',as:'top'
    get 'homes/about' => 'homes#about',as:'about'
+
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
